0442258dadcb89c50a2b4e3aea0fc934bc71e08e
henry.herman <henry.herman@2cfd8b6e-c46e-11de-b12b-e93134c88cf6>
2010-08-12 03:30:34 +0000

    Added Rotary Selection Valve

---
+++ examples/FDM/I2CValve.cpp
@@ -0,0 +1,75 @@
+
+
+#include "WProgram.h"
+#include "I2CValve.h"
+#include <stdlib.h>
+#include <Wire.h>
+
+I2CValve::I2CValve(){
+    init();
+}
+
+I2CValve::I2CValve(char addr, int sz){
+    setAddress(addr);
+    setValveSize(sz);
+    init();
+}
+
+void I2CValve::setAddress(char addr){
+    address = addr;
+}
+
+void I2CValve::setValveSize(int sz){
+	size = sz;
+}
+
+void I2CValve::init(){
+  vstatus = 0;
+}
+
+void I2CValve::reset(){
+	vstatus = 0;
+    goHome();
+}
+
+int I2CValve::getPosition(){
+    int csum = CADDRESS^SUPDATE^EMPTY;
+    byte value[2];
+    Wire.beginTransmission(address);
+    Wire.send(SUPDATE);
+    Wire.send(EMPTY);
+    Wire.send(csum);
+    Wire.endTransmission(); 
+    Wire.requestFrom(address, 2);
+    if (Wire.available()) {
+      value[0] = Wire.receive();
+    }
+    if (Wire.available()) {
+      value[1] = Wire.receive();
+    }
+	
+    vstatus = (int)value[0];
+	return vstatus;
+}
+
+void I2CValve::setPosition(int position){          
+    Wire.beginTransmission(address);
+    Wire.send(CPOS);
+    Wire.send(position);
+    Wire.send(CADDRESS^CPOS^position);
+    Wire.endTransmission();
+}
+
+void I2CValve::goHome(){
+    Wire.beginTransmission(address);
+    Wire.send(HOMEVP);
+    Wire.send(EMPTY);
+    Wire.send(CADDRESS^HOMEVP^EMPTY);
+    Wire.endTransmission();
+}
+
+boolean I2CValve::isValidPosition(int position) {
+    if (position > 0 && position <= size)
+        return true;
+    return false;
+}
---
+++ examples/FDM/I2CValve.h
@@ -0,0 +1,40 @@
+/*
+  I2Cvalve library for Wiring & Arduino
+*/
+
+#ifndef I2CValve_h
+#define I2CValve_h
+//#endif
+
+//Set arduino's TWI frequency low.
+
+#define ERR	-1
+#define CADDRESS (address<<1)
+#define CPOS	'P'
+#define HOMEVP	'M'
+#define SUPDATE	'S'
+#define EMPTY	0x00
+
+
+class I2CValve{
+  public:
+    I2CValve(); 				//Initialize a valve object (Constructor)
+    I2CValve(char addr, int sz);		//Initialize a valve object, specify address as char
+    int getPosition();    //Returns stored position from status var.
+    void setPosition(int position);
+    void goHome();
+    boolean isValidPosition(int position);
+    
+  private:
+    //Methods
+    void init();				//Private - initialize values for instance
+    void reset();				//Private - resets values in case of error
+    void setAddress(char addr); //Private - sets address variable
+    void setValveSize(int sz);  //Private - sets size variable
+    //Variables
+    int 	vstatus;//Position of valve or error if it has occurred.
+    int 	size;	//Number of positions on valve
+    char 	address;//I2C Bus Address of valve
+};
+
+#endif
